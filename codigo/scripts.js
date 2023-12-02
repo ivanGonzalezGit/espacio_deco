f7c027854c8dc8cd3152ea51049d59660b0d6b1f
ivan gonzalez <ivanfgonzalezruiz@gmail.com>
2023-12-02 11:06:16 -0300

    se arregló el navbar, el nav y el footer

--- codigo/scripts.js
+++ codigo/scripts.js
@@ -1,10 +1,12 @@
 
 function ordenarDivs() {
   const headerHTML = `
-  <p><i class="fa-brands fa-whatsapp" style="color: #034132;"></i> 11-7144-7569</p>
-  <a href="https://www.facebook.com/profile.php?id=100083780238446"><p><i class="fa-brands fa-facebook-f" style="color: #034132;"></i> Facebook</p></a>
-  <a href="https://www.instagram.com/regyespaciodeco/"><p><i class="fa-brands fa-instagram" style="color: #034132;"></i> Instagram</p></a>
-  <p id="mail">regy.espaciodeco@gmail.com</p>
+    <div class="datos">
+      <p><i class="fa-brands fa-whatsapp" style="color: #034132;"></i> 11-7144-7569</p>
+      <a href="https://www.facebook.com/profile.php?id=100083780238446"><p><i class="fa-brands fa-facebook-f" style="color: #034132;"></i> Facebook</p></a>
+      <a href="https://www.instagram.com/regyespaciodeco/"><p><i class="fa-brands fa-instagram" style="color: #034132;"></i> Instagram</p></a>
+      <p id="mail">regy.espaciodeco@gmail.com</p>
+    </div>
   `;
 
   const divfantasma1 = document.getElementById('fantasma1');
